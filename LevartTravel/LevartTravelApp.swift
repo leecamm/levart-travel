27923569330e714a63c5c9353cab4090c8d1c2a6
Duc Hieu Le <duchieule630@gmail.com>
2022-11-11 11:44:43 +0100

    Initial Commit

---
+++ LevartTravel/LevartTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevartTravelApp.swift
+//  LevartTravel
+//
+//  Created by Duc Hieu Le on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LevartTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

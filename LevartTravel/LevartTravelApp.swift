8ff9cbbb8899da99684eaf3971bce4e0c22dffa5
Duc Hieu Le <duchieule630@gmail.com>
2022-11-11 23:06:26 +0100

    feat: Loading Screen

--- LevartTravel/LevartTravelApp.swift
+++ LevartTravel/LevartTravelApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LevartTravelApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
---
+++ LevartTravel/SplashScreenView.swift
@@ -0,0 +1,57 @@
+//
+//  SplashScreenView.swift
+//  LevartTravel
+//
+//  Created by Duc Hieu Le on 11/11/2022.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            ZStack{
+                Color("BackgroundIntro")
+                    .ignoresSafeArea()
+            VStack {
+                
+                VStack {
+                    Image("Levart Travel")
+                        .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 200, height: 200)
+                                    .padding(.bottom, 20)
+                    Text("Travel is soooo Easy")
+                        .font(Font.custom("Poppins-ExtraBold", size: 28))
+                        .foregroundColor(.black.opacity(0.80))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.5)) {
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self.isActive = true
+                }
+            }
+            }
+        }
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
